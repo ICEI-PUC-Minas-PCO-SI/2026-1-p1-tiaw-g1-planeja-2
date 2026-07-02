be5158e51e56f9def11fbf15ce1c9a2bfabe06c7
christina <christina123danubia@gmail.com>
2026-07-02 17:34:42 -0300

    junção grafico e tela de metas

---
+++ src/public/Nova pasta/script.js
@@ -0,0 +1,429 @@
+document.getElementById("prazo").min =
+new Date().toISOString().split("T")[0];
+
+const form = document.getElementById("formMeta");
+const listaMetas = document.getElementById("listaMetas");
+
+let metas = JSON.parse(localStorage.getItem("metas")) || [];
+
+metas = metas.map(meta => ({
+    ...meta,
+    valorObjetivo: Number(meta.valorObjetivo),
+    valorAtual: Number(meta.valorAtual)
+}));
+
+let editando = null;
+
+function salvarLocalStorage() {
+
+    localStorage.setItem(
+        "metas",
+        JSON.stringify(metas)
+    );
+
+}
+
+let detalheAberto = null;
+let graficos = [];
+
+function mostrarMetas() {
+
+    listaMetas.innerHTML = "";
+
+    graficos.forEach(grafico => grafico.destroy());
+    graficos = [];
+
+    if (metas.length === 0) {
+
+        listaMetas.innerHTML = `
+
+        <div class="sem-metas">
+            Nenhuma meta cadastrada.
+        </div>
+
+        `;
+
+        return;
+    }
+
+    metas.forEach((meta, index) => {
+
+        let falta = meta.valorObjetivo - meta.valorAtual;
+
+        if (falta < 0) {
+            falta = 0;
+        }
+
+        const percentual =
+            meta.valorObjetivo > 0
+            ? (meta.valorAtual / meta.valorObjetivo) * 100
+        : 0;
+
+        listaMetas.innerHTML += `
+
+            <div class="card">
+
+                <div class="card-topo">
+
+                    <h3>${meta.nome}</h3>
+
+                    <div class="status">
+
+                        ${meta.valorAtual >= meta.valorObjetivo 
+                        && meta.valorObjetivo > 0
+
+                        ? "Meta concluída"
+
+                        : "Meta ativa"}
+
+                    </div>
+
+                </div>
+
+                <div class="infos">
+
+                    <div class="info-box">
+                        <p>Valor Atual</p>
+
+                        <h4>
+                        R$ ${meta.valorAtual.toFixed(2)}
+                        </h4>
+                    </div>
+
+                    <div class="info-box">
+                        <p>Valor Objetivo</p>
+
+                        <h4>
+
+                        ${meta.valorObjetivo > 0
+
+                        ? `R$ ${meta.valorObjetivo.toFixed(2)}`
+
+                        : "Não definido"}
+
+                        </h4>
+
+                    </div>
+
+                    <div class="info-box">
+                        <p>Falta</p>
+
+                        <h4>
+
+                        ${meta.valorObjetivo > 0
+
+                        ? `R$ ${falta.toFixed(2)}`
+
+                        : "Não definido"}
+
+                        </h4>
+                    </div>
+
+                    <div class="info-box">
+                        <p>Prazo</p>
+
+                        <h4>
+                            ${meta.prazo || "Não definido"}
+                        </h4>
+                    </div>
+
+                </div>
+
+                <div class="barra">
+                    <div class="progresso" style="width: ${percentual}%"></div>
+                </div>
+
+                <p class="percentual">
+                    ${percentual.toFixed(0)}% concluído
+                </p>
+
+
+
+                <button class="btn-detalhes" onclick="abrirDetalhes(${index})">
+                    ${
+                        detalheAberto === index
+                        ? "Ocultar detalhes"
+                        : "Ver detalhes"
+                    }
+                </button>
+
+                ${
+                    detalheAberto === index
+                    ? `
+                        <div class="detalhes-meta">
+                            
+                            <h3 class="titulo-grafico">Evolução da Meta</h3>
+
+                            <canvas id="grafico-${index}" class="grafico-meta"></canvas>
+
+                            <div class="adicionar-valor">
+
+                                <input
+                                type="number"
+                                id="valor-${index}"
+                                placeholder="Digite o valor guardado"
+                                >
+
+                                <button
+                                    class="btn-add"
+                                    onclick="adicionarValor(${index})"
+                                >
+                                    + Adicionar
+                                </button>
+
+                            </div>
+
+                            <div class="acoes">
+
+                                <button
+                                class="btn-editar"
+                                onclick="editarMeta(${index})"
+                                >
+                                    Editar
+                                </button>
+
+                                <button
+                                    class="btn-excluir"
+                                    onclick="excluirMeta(${index})"
+                                >
+                                    Excluir
+                                </button>
+
+                            </div>
+
+                        </div>
+                    `
+                    : ""
+                }
+            </div>
+        `;
+    });
+    
+    if (detalheAberto !== null) {
+        criarGrafico(detalheAberto);
+    }
+}
+
+function abrirDetalhes(index) {
+    if (detalheAberto === index) {
+        detalheAberto = null;
+    } else {
+        detalheAberto = index;
+    }
+
+    mostrarMetas();
+}
+
+function criarGrafico(index) {
+    const meta = metas[index];
+
+    const ctx = document.getElementById(`grafico-${index}`);
+
+    if (!ctx) return;
+
+    const grafico = new Chart(ctx, {
+        type: "line",
+
+        data: {
+            labels: meta.historico.map(item => item.data),
+
+            datasets: [
+                {
+                    label: "Evolução da Meta",
+
+                    data: meta.historico.map(item => item.valor),
+
+                    borderColor: "#2563EB",
+
+                    backgroundColor: "#DBEAFE",
+
+                    borderWidth: 3,
+
+                    tension: 0.3
+                }
+            ]
+        },
+
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+    graficos.push(grafico);
+}
+
+form.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    const nomeMeta =
+    document.getElementById("nomeMeta")
+    .value
+    .trim();
+
+    const valorObjetivo = Number(
+        document.getElementById("valorObjetivo").value
+    ) || 0;
+
+    const prazo =
+    document.getElementById("prazo").value;
+
+    if (nomeMeta === "") {
+
+        alert(
+            "O nome da meta é obrigatório."
+        );
+
+        return;
+    }
+
+    const meta = {
+
+        nome: nomeMeta,
+
+        valorObjetivo: valorObjetivo,
+
+        prazo: prazo,
+
+        valorAtual: 0,
+
+        historico: [
+            {
+                data: new Date().toLocaleDateString(),
+                valor: 0
+            }
+        ]
+    };
+
+    if (editando !== null) {
+
+        meta.valorAtual = metas[editando].valorAtual;
+        meta.historico = metas[editando].historico;
+        metas[editando] = meta;
+
+        editando = null;
+
+    } else {
+
+        metas.push(meta);
+
+    }
+
+    salvarLocalStorage();
+
+    mostrarMetas();
+
+    form.reset();
+
+});
+
+function adicionarValor(index) {
+
+    const inputValor =
+    document.getElementById(`valor-${index}`);
+
+    const valor = Number(inputValor.value);
+
+    if (valor === 0) {
+
+        alert(
+            "Digite um valor diferente de zero."
+        );
+
+        return;
+    }
+
+    if(metas[index].valorAtual + valor < 0){
+
+        alert("O valor atual não pode ficar negativo.");
+
+        return;
+    }
+
+    metas[index].valorAtual += valor;
+
+    metas[index].historico.push({
+    data: new Date().toLocaleDateString(),
+    valor: metas[index].valorAtual
+    });
+
+    salvarLocalStorage();
+
+    mostrarMetas();
+
+}
+
+function excluirMeta(index) {
+
+    const confirmar = confirm(
+        "Deseja excluir esta meta?"
+    );
+
+    if (confirmar) {
+
+        metas.splice(index, 1);
+
+        salvarLocalStorage();
+
+        mostrarMetas();
+
+    }
+
+}
+
+function editarMeta(index) {
+
+    const meta = metas[index];
+
+    document.getElementById("nomeMeta").value =
+    meta.nome;
+
+    document.getElementById("valorObjetivo").value =
+    meta.valorObjetivo;
+
+    document.getElementById("prazo").value =
+    meta.prazo;
+
+    editando = index;
+
+    window.scrollTo({
+
+        top: 0,
+
+        behavior: "smooth"
+
+    });
+
+}
+
+/*BARRA DE PROGRESSO*/
+
+function mostrarPercentual(percentual) {
+
+    alert(`Você já concluiu ${percentual}% da sua meta.`);
+
+}
+
+function limparFormulario() {
+
+    form.reset();
+
+    editando = null;
+
+}
+
+mostrarMetas();
+
+
+
+     
+   
+
+
