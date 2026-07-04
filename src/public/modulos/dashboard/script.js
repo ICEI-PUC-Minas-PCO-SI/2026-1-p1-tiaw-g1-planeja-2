d21b77995b9e18ef9230bf299e5313536398d978
Victoria <vicvaldirene@gmail.com>
2026-07-04 19:02:55 -0300

    Pastas das funcionalidades

---
+++ src/public/modulos/dashboard/script.js
@@ -0,0 +1,219 @@
+// Botões dos meses
+const botoesMes = document.querySelectorAll(".mes-btn");
+
+// mes atual
+let mesAtual = "jan";
+
+//dias referente ao mes
+const meses = {
+
+    jan: 0,
+    fev: 1,
+    mar: 2,
+    abr: 3,
+    mai: 4,
+    jun: 5,
+    jul: 6,
+    ago: 7,
+    set: 8,
+    out: 9,
+    nov: 10,
+    dez: 11
+
+};
+
+// dados financeiros
+let dadosFinanceiros;
+
+
+// Converção modeda
+function formatarMoeda(valor) {
+
+    return valor.toLocaleString("pt-BR", {
+
+        style: "currency",
+        currency: "BRL"
+
+    });
+
+}
+
+// atualizar a tela
+
+function atualizarDashboard() {
+
+    let totalEntradas = 0;
+    let totalSaidas = 0;
+
+    for (let i = 0; i < dadosFinanceiros.transacoes.length; i++) {
+
+        const transacao = dadosFinanceiros.transacoes[i];
+
+        const data = new Date(transacao.data);
+
+        if (data.getMonth() === meses[mesAtual]) {
+
+            if (transacao.tipo === "entrada") {
+
+                totalEntradas += transacao.valor;
+
+            } else {
+
+                totalSaidas += transacao.valor;
+
+            }
+
+        }
+
+    }
+
+    const saldoAtual = totalEntradas - totalSaidas;
+
+    document.getElementById("entradaValor").innerText =
+        formatarMoeda(totalEntradas);
+
+    document.getElementById("saidaValor").innerText =
+        formatarMoeda(totalSaidas);
+
+    document.getElementById("saldoAtual").innerText =
+        formatarMoeda(saldoAtual);
+
+}
+
+
+// troca do botao ativo
+
+for (let i = 0; i < botoesMes.length; i++) {
+
+    botoesMes[i].addEventListener("click", function () {
+
+        // REMOVE AZUL DE TODOS
+        for (let j = 0; j < botoesMes.length; j++) {
+
+            botoesMes[j].classList.remove("ativo");
+
+        }
+
+        // ADICIONA AZUL NO CLICADO
+        this.classList.add("ativo");
+
+        // PEGA O MÊS DO BOTÃO
+        mesAtual = this.dataset.mes;
+
+        // ATUALIZA TELA
+        atualizarDashboard();
+
+    });
+
+}
+
+
+
+//Grafico 
+function criarGrafico() {
+
+    const entradasMes = Array(12).fill(0);
+    const saidasMes = Array(12).fill(0);
+
+    for (let i = 0; i < dadosFinanceiros.transacoes.length; i++) {
+
+        const transacao = dadosFinanceiros.transacoes[i];
+
+        const data = new Date(transacao.data);
+
+        const mes = data.getMonth();
+
+        if (transacao.tipo === "entrada") {
+
+            entradasMes[mes] += transacao.valor;
+
+        } else {
+
+            saidasMes[mes] += transacao.valor;
+
+        }
+
+    }
+
+    const ctx = document.getElementById("myChart");
+
+    new Chart(ctx, {
+
+        type: "bar",
+
+        data: {
+
+            labels: [
+                "JAN",
+                "FEV",
+                "MAR",
+                "ABR",
+                "MAI",
+                "JUN",
+                "JUL",
+                "AGO",
+                "SET",
+                "OUT",
+                "NOV",
+                "DEZ"
+            ],
+
+            datasets: [
+
+                {
+                    label: "Entradas",
+                    data: entradasMes,
+                    backgroundColor: "#22C55E",
+                    borderRadius: 6
+                },
+
+                {
+                    label: "Saídas",
+                    data: saidasMes,
+                    backgroundColor: "#EF4444",
+                    borderRadius: 6
+                }
+            ]
+        },
+
+        options: {
+
+            responsive: true,
+
+            plugins: {
+
+                legend: {
+                    position: "top"
+                }
+            },
+
+            scales: {
+                y: {
+
+                    beginAtZero: true,
+
+                    grid: {
+
+                        color: "#E5E7EB"
+
+                    }
+                }
+            }
+        }
+    });
+}
+
+async function carregarDados() {
+
+    const resposta = await fetch("transacoes.json");
+
+    dadosFinanceiros = await resposta.json();
+
+    atualizarDashboard();
+
+    criarGrafico();
+
+}
+
+
+carregarDados();
---
+++ src/public/modulos/metas/script.js
@@ -0,0 +1,304 @@
+document.getElementById("prazo").min =
+new Date().toISOString().split("T")[0];
+
+const form = document.getElementById("formMeta");
+const listaMetas = document.getElementById("listaMetas");
+
+let metas = JSON.parse(localStorage.getItem("metas")) || [];
+
+metas = metas.map(meta => ({
+    ...meta,
+    valorObjetivo: Number(meta.valorObjetivo),
+    valorAtual: Number(meta.valorAtual)
+}));
+
+let editando = null;
+
+function salvarLocalStorage() {
+
+    localStorage.setItem(
+        "metas",
+        JSON.stringify(metas)
+    );
+
+}
+
+function mostrarMetas() {
+
+    listaMetas.innerHTML = "";
+
+    if (metas.length === 0) {
+
+        listaMetas.innerHTML = `
+
+        <div class="sem-metas">
+            Nenhuma meta cadastrada.
+        </div>
+
+        `;
+
+        return;
+    }
+
+    metas.forEach((meta, index) => {
+
+        let falta = meta.valorObjetivo - meta.valorAtual;
+
+        if (falta < 0) {
+            falta = 0;
+        }
+
+        listaMetas.innerHTML += `
+
+        <div class="card">
+
+            <div class="card-topo">
+
+                <h3>${meta.nome}</h3>
+
+                <div class="status">
+
+                ${meta.valorAtual >= meta.valorObjetivo 
+                && meta.valorObjetivo > 0
+
+                ? "Meta concluída"
+
+                : "Meta ativa"}
+
+                </div>
+
+            </div>
+
+            <div class="infos">
+
+                <div class="info-box">
+                    <p>Valor Atual</p>
+
+                    <h4>
+                    R$ ${meta.valorAtual.toFixed(2)}
+                    </h4>
+                </div>
+
+                <div class="info-box">
+                    <p>Valor Objetivo</p>
+
+                    <h4>
+
+                    ${meta.valorObjetivo > 0
+
+                    ? `R$ ${meta.valorObjetivo.toFixed(2)}`
+
+                    : "Não definido"}
+
+                    </h4>
+
+                </div>
+
+                <div class="info-box">
+                    <p>Falta</p>
+
+                    <h4>
+
+                    ${meta.valorObjetivo > 0
+
+                    ? `R$ ${falta.toFixed(2)}`
+
+                : "Não definido"}
+
+                    </h4>
+                </div>
+
+                <div class="info-box">
+                    <p>Prazo</p>
+
+                    <h4>
+                    ${meta.prazo || "Não definido"}
+                    </h4>
+                </div>
+
+            </div>
+
+            <div class="adicionar-valor">
+
+                <input
+                    type="number"
+                    id="valor-${index}"
+                    placeholder="Digite o valor guardado"
+                >
+
+                <button
+                    class="btn-add"
+                    onclick="adicionarValor(${index})"
+                >
+                    + Adicionar
+                </button>
+
+            </div>
+
+            <div class="acoes">
+
+                <button
+                    class="btn-editar"
+                    onclick="editarMeta(${index})"
+                >
+                    Editar
+                </button>
+
+                <button
+                    class="btn-excluir"
+                    onclick="excluirMeta(${index})"
+                >
+                    Excluir
+                </button>
+
+            </div>
+
+        </div>
+
+        `;
+    });
+
+}
+
+form.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    const nomeMeta =
+    document.getElementById("nomeMeta")
+    .value
+    .trim();
+
+    const valorObjetivo = Number(
+        document.getElementById("valorObjetivo").value
+    ) || 0;
+
+    const prazo =
+    document.getElementById("prazo").value;
+
+    if (nomeMeta === "") {
+
+        alert(
+            "O nome da meta é obrigatório."
+        );
+
+        return;
+    }
+
+    const meta = {
+
+        nome: nomeMeta,
+
+        valorObjetivo: valorObjetivo,
+
+        prazo: prazo,
+
+        valorAtual: 0
+    };
+
+    if (editando !== null) {
+
+        meta.valorAtual =
+        metas[editando].valorAtual;
+
+        metas[editando] = meta;
+
+        editando = null;
+
+    } else {
+
+        metas.push(meta);
+
+    }
+
+    salvarLocalStorage();
+
+    mostrarMetas();
+
+    form.reset();
+
+});
+
+function adicionarValor(index) {
+
+    const inputValor =
+    document.getElementById(`valor-${index}`);
+
+    const valor = Number(inputValor.value);
+
+    if (valor === 0) {
+
+        alert(
+            "Digite um valor diferente de zero."
+        );
+
+        return;
+    }
+
+    if(metas[index].valorAtual + valor < 0){
+
+        alert("O valor atual não pode ficar negativo.");
+
+        return;
+    }
+
+    metas[index].valorAtual += valor;
+
+    salvarLocalStorage();
+
+    mostrarMetas();
+
+}
+
+function excluirMeta(index) {
+
+    const confirmar = confirm(
+        "Deseja excluir esta meta?"
+    );
+
+    if (confirmar) {
+
+        metas.splice(index, 1);
+
+        salvarLocalStorage();
+
+        mostrarMetas();
+
+    }
+
+}
+
+function editarMeta(index) {
+
+    const meta = metas[index];
+
+    document.getElementById("nomeMeta").value =
+    meta.nome;
+
+    document.getElementById("valorObjetivo").value =
+    meta.valorObjetivo;
+
+    document.getElementById("prazo").value =
+    meta.prazo;
+
+    editando = index;
+
+    window.scrollTo({
+
+        top: 0,
+
+        behavior: "smooth"
+
+    });
+
+}
+
+function limparFormulario() {
+
+    form.reset();
+
+    editando = null;
+
+}
+
+mostrarMetas();
